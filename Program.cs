7b697d79a9a600cd10b0f0fec80587c480b3f7d7
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 21:36:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Cart
+{
+    private static Cart instance;
+    public int Count;
+
+    private Cart() { }
+
+    public static Cart GetInstance()
+    {
+        if (instance == null)
+            instance = new Cart();
+        return instance;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var cart1 = Cart.GetInstance();
+        var cart2 = Cart.GetInstance();
+
+        cart1.Count++;
+        Console.WriteLine(cart2.Count); // той самий об’єкт
+    }
+}
+
+
+
+
+
+
